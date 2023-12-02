3e51f128f96af7ead970f40701811b96bbd285e9
laamyry <slamiri10@gmail.com>
2023-12-02 01:09:32 +0100

    update everything

--- README.md
+++
@@ -1,1 +0,0 @@
-medlaamyry {contact@medlaamyry.com}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greeny: {
+        300: '#11D28C'
+        },
+        darky: {
+        700: '#303030'
+        },
+        lighty: {
+        100: '#FCFCFC'
+        }
+      },
+      fontFamily: {
+        'us-thin': ['Us Thin', 'sans-serif'],
+        'us-light': ['Us Light', 'sans-serif'],
+        'us-heavy': ['US Heavy', 'sans-serif'],
+        'us-bold': ['Us Bold', 'sans-serif'],
+        'us-semi-bold': ['Us SemiBold', 'sans-serif'],
+        'us-regular': ['Us Regular', 'sans-serif'],
+        'us-book': ['Us Book', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
