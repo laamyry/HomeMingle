691a3b8a9ab8ac82734580b5080af555009c4e95
laamyry <slamiri10@gmail.com>
2023-12-19 11:09:26 +0100

    add form

--- components/StepsForm1.vue
+++ components/StepsForm1.vue
@@ -1,52 +1,83 @@
 <template>
-    <div class="flex flex-col space-y-4">
-        <div class="flex justify-between items-center space-x-4">
-            <div
-                v-for="(step, index) in steps"
-                :key="index"
-                class="step"
-                :class="{ active: index === currentStep }"
-            >
-                <div @click="currentStep = index" class="cursor-pointer">
-                    <span
-                        class="absolute top-0 left-0 w-0 h-full bg-transparent group-hover:bg-gray-200 lg:w-full lg:h-0 lg:bottom-0 lg:top-auto"
-                        aria-hidden="true"
-                    />
+    <div class="flex flex-col bg-[#E0E0E0]">
 
-                    <span
-                        class="px-6 py-5 flex items-start text-sm font-medium"
-                    >
-                        <span class="flex-shrink-0">
-                            <span
-                                class="w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full"
-                            >
+        <div class="flex justify-center gap-10 w-[80%] mx-auto">
+            <div class="flex justify-between items-center space-x-72">
+                <div v-for="(step, index) in steps" :key="index" class="step" :class="{ active: index === currentStep }">
+                    <div @click="currentStep = index" class="cursor-pointer">
+                        <span
+                            class="absolute top-0 left-0 w-0 h-full bg-transparent group-hover:bg-gray-200 lg:w-full lg:h-0 lg:bottom-0 lg:top-auto"
+                            aria-hidden="true" />
+
+                        <span class="px-6 py-5 flex items-start text-sm font-medium">
+                            <span class="flex-shrink-0">
+                                <span class="fl w-10 h-10 flex items-center justify-center bg-greeny-300 rounded-full">
+                                </span>
+                            </span>
+                            <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
+                                <span class="text-xs font-semibold tracking-wide uppercase">{{ step.name }}</span>
+                                <span class="text-sm font-medium text-gray-500">{{ step.description }}</span>
                             </span>
                         </span>
-                        <span class="mt-0.5 ml-4 min-w-0 flex flex-col">
-                            <span
-                                class="text-xs font-semibold tracking-wide uppercase"
-                                >{{ step.name }}</span
-                            >
-                            <span class="text-sm font-medium text-gray-500">{{
-                                step.description
-                            }}</span>
-                        </span>
-                    </span>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="flex items-center mx-auto w-[80%] rounded-lg p-9 text-center">
+            <p class="self-center text-center">fill your information</p>
+        </div>
+        <div class="flex flex-col ">
+            <div v-if="currentStep === 0">
+                <div id="first-tab" class="bg-lighty-100 w-[80%] rounded-lg mx-auto p-9">
 
-        <div v-if="currentStep === 0">this is first step</div>
-        <div v-if="currentStep === 1">this is second step</div>
-        <div v-if="currentStep === 2">this is third step</div>
+                    <p class="py-3 ml-8 font-us-regular text-xl">Your task location</p>
+                    <div class="flex justify-center mx-auto focus:outline-none">
+                        <input type="text" class="focus:ring-transparent w-[95%] h-16 rounded border-[#00110A] " />
 
-        <div class="flex justify-between">
-            <button @click="prevStep" :disabled="currentStep === 0">
-                Previous
-            </button>
-            <button @click="nextStep" :disabled="currentStep === 2">
-                Next
-            </button>
+                    </div>
+                </div>
+                <div id="second-tab" class="bg-lighty-100 w-[80%] rounded-lg mx-auto p-9 mt-6">
+                    <p class="py-3 ml-8 font-us-regular text-xl">Task options</p>
+                    <div class=" mx-auto">
+
+                        <fieldset class="px-5 ml-8 font-us-regular text-base">
+                            <legend class="font-us-semi-bold text-base">How big is your task?</legend>
+                            <div class="flex items-center gap-1.5">
+                                <input type="radio" name="service" id="Small" class="text-greeny-300 focus:ring-transparent"
+                                    value="Small" />
+                                <label for="apple">Small - Est. 1 hr</label>
+                            </div>
+                            <div class="flex items-center gap-1.5">
+                                <input type="radio" name="service" id="Medium"
+                                    class="text-greeny-300 focus:ring-transparent" value="Medium" />
+                                <label for="mango">Medium - Est. 2-3 hrs</label>
+                            </div>
+                            <div class="flex items-center gap-1.5">
+                                <input type="radio" name="service" id="Large" class="text-greeny-300 focus:ring-transparent"
+                                    value="Large" />
+                                <label for="banana"> Large - Est. 4+ hrs</label>
+                            </div>
+                        </fieldset>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div v-if="currentStep === 1">this is second step</div>
+            <div v-if="currentStep === 2">this is third step</div>
+
+        </div>
+        <div class="flex justify-center w-[80%] mx-auto">
+            <div class="flex justify-between w-full space-x-72 my-3">
+                <button class="bg-greeny-300 p-2 w-20 rounded-md" @click="prevStep" :disabled="currentStep === 0">
+                    Previous
+                </button>
+                <button class="bg-greeny-300 p-2 w-20 rounded-md" @click="nextStep" :disabled="currentStep === 2">
+                    Next
+                </button>
+            </div>
         </div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,155 +1,163 @@
 <!-- start of the template -->
 <template class="p-0 m-0">
-  <div class="selection:bg-greeny-300">
-    <div class="w-[25%] flex items-center mx-auto justify-center font-us-bold uppercase line-clamp-2 text-center mt-20 text-2xl text-darky-700">
-      <p>Where Skills Meet Spaces, Crafting Homes to Perfection</p>
-    </div>
-    <!-- search bar with two images -->
-    <div id="container" class="flex items-center">
-      <div id="search" class="flex justify-between items-center float-left w-[80%] mx-auto">
-        <div id="image1">
-          <img src="/images/hammer.png" class="w-52" alt="hammer" />
+    <div class="selection:bg-greeny-300">
+        <div
+            class="w-[25%] flex items-center mx-auto justify-center font-us-bold uppercase line-clamp-2 text-center mt-20 text-2xl text-darky-700">
+            <p>Where Skills Meet Spaces, Crafting Homes to Perfection</p>
         </div>
-        <!-- search bar -->
-        <div id="search" class="max-w-md mx-auto w-[40%]">
-          <div class="p-2 border border-[#303030] rounded-full relative">
-            <input type="search" placeholder="Services near me" class="px-3 bg-transparent border-transparent outline-none w-[92%]" />
-            <Icon name="fa6-solid:magnifying-glass" class="text-greeny-300" />
-          </div>
-        </div>
-        <div id="image2">
-          <img src="/images/drill.png" class="w-52" alt="drill" />
-        </div>
-      </div>
-    </div>
-    <!-- services title-->
-    <div id="title" class="text-center font-us-bold uppercase text-darky-700">
-      <h2 class="underline decoration-4 underline-offset-4 mb-14">Trending services</h2>
-    </div>
-    <!-- services section using js to create-->
-    <div id="car_container" class="flex justify-center flex-row items-center mx-auto max-w-md gap-5">
-      <span v-for="item in services">
-        <div id="card" class="flex flex-col w-64 text-center justify-center">
-          <div id="image" class="shadow-lg">
-            <img :src="item.image" :alt="item.title" />
-          </div>
-          <div :id="'id' + item.id" class="w-60 self-center bg-[#EAEAEA] rounded-b-md shadow-md p-1">
-            <div id="titl" class="font-us-semi-bold text-md uppercase text-darky-700">
-              {{ item.title }}
+        <!-- search bar with two images -->
+        <div id="container" class="flex items-center">
+            <div id="search" class="flex justify-between items-center float-left w-[80%] mx-auto">
+                <div id="image1">
+                    <img src="/images/hammer.png" class="w-52" alt="hammer" />
+                </div>
+                <!-- search bar -->
+                <div id="search" class="max-w-md mx-auto w-[40%]">
+                    <div class="p-2 border border-[#303030] rounded-full relative">
+                        <input type="search" placeholder="Services near me"
+                            class="px-3 bg-transparent border-transparent outline-none w-[92%]" />
+                        <Icon name="fa6-solid:magnifying-glass" class="text-greeny-300" />
+                    </div>
+                </div>
+                <div id="image2">
+                    <img src="/images/drill.png" class="w-52" alt="drill" />
+                </div>
             </div>
-            <div id="price" class="font-us-regular text-darky-700">
-              {{ 'Starting at '}} <span class="text-greeny-300 font-us-bold">{{ item.price + '€' }}</span>
-            </div>
-          </div>
         </div>
-      </span>
-    </div>
-    <!-- review title -->
-    <div id="title" class="text-center font-us-bold uppercase text-darky-700">
-      <h2 class="underline decoration-4 underline-offset-4 mt-16">customers talking</h2>
-    </div>
-    <!-- review section using js to create-->
-    <div class="w-full flex items-center justify-center mx-auto mt-2">
-      <figure class="flex justify-center flex-row mx-auto gap-16 mt-9 self-center">
-        <span v-for="item in review" :key="item.id">
-          <div class="flex flex-row items-center">
-            <div class="flex items-center">
-              <p class="pe-3 font-us-semi-bold text-darky-700 text-2xl">{{ item.username }}</p>
-            </div>
+        <!-- services title-->
+        <div id="title" class="text-center font-us-bold uppercase text-darky-700">
+            <h2 class="underline decoration-4 underline-offset-4 mb-14">Trending services</h2>
+        </div>
+        <!-- services section using js to create-->
+        <div id="car_container" class="flex justify-center flex-row items-center mx-auto max-w-md gap-5">
+            <span v-for="item in services" :key="item.id">
+                <div id="card" class="flex flex-col w-64 text-center justify-center">
+                    <div id="image" class="shadow-lg">
+                        <img :src="item.image" :alt="item.title" />
+                    </div>
+                    <div :id="'id' + item.id" class="w-60 self-center bg-[#EAEAEA] rounded-b-md shadow-md p-1">
+                        <div id="titl" class="font-us-semi-bold text-md uppercase text-darky-700">
+                            {{ item.title }}
+                        </div>
+                        <div id="price" class="font-us-regular text-darky-700">
+                            {{ 'Starting at ' }} <span class="text-greeny-300 font-us-bold">{{ item.price + '€' }}</span>
+                        </div>
+                    </div>
+                </div>
+            </span>
+        </div>
+        <!-- review title -->
+        <div id="title" class="text-center font-us-bold uppercase text-darky-700">
+            <h2 class="underline decoration-4 underline-offset-4 mt-16">customers talking</h2>
+        </div>
+        <!-- review section using js to create-->
+        <div class="w-full flex items-center justify-center mx-auto mt-2">
+            <figure class="flex justify-center flex-row mx-auto gap-16 mt-9 self-center">
+                <span v-for="item in review" :key="item.id">
+                    <div class="flex flex-row items-center">
+                        <div class="flex items-center">
+                            <p class="pe-3 font-us-semi-bold text-darky-700 text-2xl">{{ item.username }}</p>
+                        </div>
 
-            <div class="flex items-center text-greeny-300">
-              <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-              </svg>
-            </div>
-          </div>
+                        <div class="flex items-center text-greeny-300">
+                            <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 22 20">
+                                <path
+                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 22 20">
+                                <path
+                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 22 20">
+                                <path
+                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg class="w-5 h-5 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 22 20">
+                                <path
+                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                viewBox="0 0 22 20">
+                                <path
+                                    d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                            </svg>
+                        </div>
+                    </div>
 
-          <blockquote class="w-72 text-justify">
-            <p class="text-1xl font-us-regular text-darky-700">"{{ item.review }}"</p>
-          </blockquote>
-          <figcaption class="flex items-center mt-6 space-x-3 rtl:space-x-reverse"></figcaption>
-        </span>
-      </figure>
+                    <blockquote class="w-72 text-justify">
+                        <p class="text-1xl font-us-regular text-darky-700">"{{ item.review }}"</p>
+                    </blockquote>
+                    <figcaption class="flex items-center mt-6 space-x-3 rtl:space-x-reverse"></figcaption>
+                </span>
+            </figure>
+        </div>
     </div>
-  </div>
-  
 </template>
 <!-- end of the template -->
 <!-- start of js -->
 <script lang="ts" setup>
 const services = [
-  {
-    id: 1,
-    title: 'Home Cleaning',
-    image: '/images/cleaning.jpg',
-    price: 80,
-  },
-  {
-    id: 2,
-    title: 'Security Installation',
-    image: '/images/sec.jpg',
-    price: 150,
-  },
-  {
-    id: 3,
-    title: 'Electrical Maintenance',
-    image: '/images/electric.jpg',
-    price: 90,
-  },
-  {
-    id: 4,
-    title: 'Furniture Assembly',
-    image: '/images/furniture.jpeg',
-    price: 60,
-  },
-  {
-    id: 5,
-    title: 'Interior Painting',
-    image: '/images/paint.jpg',
-    price: 120,
-  },
+    {
+        id: 1,
+        title: 'Home Cleaning',
+        image: '/images/cleaning.jpg',
+        price: 80,
+    },
+    {
+        id: 2,
+        title: 'Security Installation',
+        image: '/images/sec.jpg',
+        price: 150,
+    },
+    {
+        id: 3,
+        title: 'Electrical Maintenance',
+        image: '/images/electric.jpg',
+        price: 90,
+    },
+    {
+        id: 4,
+        title: 'Furniture Assembly',
+        image: '/images/furniture.jpeg',
+        price: 60,
+    },
+    {
+        id: 5,
+        title: 'Interior Painting',
+        image: '/images/paint.jpg',
+        price: 120,
+    },
 ]
 
 const review = [
-  {
-    id: 1,
-    username: 'Sarah J',
-    review: 'Home Mingle Hub made finding a skilled plumber so easy! The platform is user-friendly, and I quickly connected with a reliable professional. The service was prompt, and the plumber did an excellent job. Highly recommend!',
-  },
-  {
-    id: 2,
-    username: 'Mike R',
-    review: 'Used Home Mingle Hub for electrical repairs, and it was a great experience. The professional was knowledgeable and fixed the issue efficiently. The only suggestion would be to add more professionals in my area.',
-  },
-  {
-    id: 3,
-    username: 'Alex M',
-    review: "Home Mingle Hub is a game-changer for home services. I found an amazing landscaper for my garden. The process was smooth, but I wish there were more details about each professional's expertise on their profiles.",
-  },
-  {
-    id: 4,
-    username: 'Med L',
-    review: "Aleksandr was fantastic! He came with all the equipment to do the job, even the hardware I didn't know I would need. He hung a heavy chandelier perfectly and fixed a cabinet to our wall. 100% would hire",
-  },
+    {
+        id: 1,
+        username: 'Sarah J',
+        review: 'Home Mingle Hub made finding a skilled plumber so easy! The platform is user-friendly, and I quickly connected with a reliable professional. The service was prompt, and the plumber did an excellent job. Highly recommend!',
+    },
+    {
+        id: 2,
+        username: 'Mike R',
+        review: 'Used Home Mingle Hub for electrical repairs, and it was a great experience. The professional was knowledgeable and fixed the issue efficiently. The only suggestion would be to add more professionals in my area.',
+    },
+    {
+        id: 3,
+        username: 'Alex M',
+        review: "Home Mingle Hub is a game-changer for home services. I found an amazing landscaper for my garden. The process was smooth, but I wish there were more details about each professional's expertise on their profiles.",
+    },
+    {
+        id: 4,
+        username: 'Med L',
+        review: "Aleksandr was fantastic! He came with all the equipment to do the job, even the hardware I didn't know I would need. He hung a heavy chandelier perfectly and fixed a cabinet to our wall. 100% would hire",
+    },
 ]
 </script>
 <!-- end of js -->
 <!-- optional css -->
-<style>
-#search {
-  margin-top: -25px;
-}
-
-</style>
+<style>#search {
+    margin-top: -25px;
+}</style>
